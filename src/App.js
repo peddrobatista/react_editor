34f228db1f191317152f1527523780743be0c866
pedro-batista <peddrobatista2022@gmail.com>
2022-11-04 13:33:43 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Editor from './components/Editor';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      <h1>Editor de Texto</h1>
+      <Editor/>
     </div>
-  );
+  )
 }
-
 export default App;
